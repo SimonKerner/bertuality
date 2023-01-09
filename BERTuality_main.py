acbb1a006762123188b800da106fc05d0d571cc5
Seliion <104328359+Seliion@users.noreply.github.com>
2023-01-09 09:51:22 +0100

    tests

--- BERTuality_main.py
+++ BERTuality_main.py
@@ -178,19 +178,27 @@ learn_new_token(sample, model, tokenizer)
 # get NER keywords
 ner_keywords = ner_keywords(sample)
 
+creator = keyword_creator(sample[0])
+
+
 # load news from guardian and news_api
-news_api_query, guardian_query, guardian_query_df = news_loader('2022-09-05', 'obama president')
+news_api_query, guardian_query, guardian_query_df = news_loader('2022-12-05', 'obama president')
 filtered_query = sentence_converter(news_api_query, guardian_query)
 merged_query = merge_pages(filtered_query, tokenizer)
 
 #filter information out of full article list
 info_query = filter_list_final(merged_query, ner_keywords)
 
+info_query_2 = []
+for i in creator:
+    query = filter_list_final(merged_query, i)
+    info_query_2. append(query)
+
 # focus on relevant part of sentence
 #focus_query = keyword_focus(info_query, key_words, 5)
 
 # make prediction
-query_pred = make_predictions(sample[0], info_query, model, tokenizer)
+# query_pred = make_predictions(sample[0], info_query, model, tokenizer)
 
 
 
